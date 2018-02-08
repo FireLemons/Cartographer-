e4967081824d54b6c3ab878f29c5dfdd1a9bbc18
FireLemons <shenyang64@gmail.com>
2018-02-08 11:03:01 -0600

    forgot to add script files

---
+++ Mapeople/www/js/config.js
@@ -0,0 +1,21 @@
+// Initialize Will's Firebase
+/*var config = {
+	apiKey: "AIzaSyBrjkpdRRDhQE554qEmWHAeMbrlh_lIIX8",
+	authDomain: "mapeople-558c8.firebaseapp.com",
+	databaseURL: "https://mapeople-558c8.firebaseio.com",
+	projectId: "mapeople-558c8",
+	storageBucket: "mapeople-558c8.appspot.com",
+	messagingSenderId: "104232274398"
+};*/
+
+// Initialize Mapeople Firebase
+var config = {
+	apiKey: "AIzaSyBrebIOvJpdsYgFmfy3APrhqsG45eRTpfo",
+	authDomain: "mapeople-7bcd9.firebaseapp.com",
+	databaseURL: "https://mapeople-7bcd9.firebaseio.com",
+	projectId: "mapeople-7bcd9",
+	storageBucket: "",
+	messagingSenderId: "282260381580"
+};
+
+firebase.initializeApp(config);
---
+++ Mapeople/www/js/login.js
@@ -0,0 +1,49 @@
+if(!firebase){
+	console.error("Firebase API has failed to load on this page.");
+} else {
+	 
+	var uiConfig = {
+		callbacks: {
+			signInSuccess: function(currentUser, credential, redirectUrl) {
+				dbRef.child("users/" + currentUser.uid).once("value").then(function(snapshot){
+					if(!(snapshot.val() && snapshot.val().username)){
+						dbRef.child("users/" + currentUser.uid).set({
+							username: currentUser.displayName,
+							email: currentUser.email
+						});
+					}
+	 
+					//Manual redirect.
+					/*window.location.replace("???.html");*/
+				});
+	 
+				//Returns false for manual redirect to allow time for firebase query to run.
+				return false;
+			}
+		},
+		signInOptions: [
+			// Leave the lines as is for the providers you want to offer your users.
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID
+		],
+		// Terms of service url.
+		tosUrl: '<your-tos-url>'
+	};
+	
+	// Initialize the FirebaseUI Widget using Firebase.
+	var ui = new firebaseui.auth.AuthUI(firebase.auth());
+	// The start method will wait until the DOM is loaded.
+	ui.start('#firebaseui-auth-container', uiConfig);
+	
+	firebase.auth().onAuthStateChanged(function(user){
+		if(user){
+			//User successfully logged in
+		} else {
+			displayAnonymous();
+		}
+	});
+}
+
+function displayAnonymous(){
+	
+	
+}
