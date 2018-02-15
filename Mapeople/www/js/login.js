6a52cd410dfeeccf047eee131bd87d50cb8df5e5
FireLemons <shenyang64@gmail.com>
2018-02-15 16:08:44 -0600

    login with google working for static files

--- Mapeople/www/js/login.js
+++ Mapeople/www/js/login.js
@@ -3,38 +3,7 @@ if(!firebase){
 } else if(!jQuery){
 	console.error("JQuery failed to load on this page.");
 } else {
-	 
-	var uiConfig = {
-		callbacks: {
-			signInSuccess: function(currentUser, credential, redirectUrl) {
-				dbRef.child("users/" + currentUser.uid).once("value").then(function(snapshot){
-					if(!(snapshot.val() && snapshot.val().username)){
-						dbRef.child("users/" + currentUser.uid).set({
-							username: currentUser.displayName,
-							email: currentUser.email
-						});
-					}
-	 
-					//Manual redirect.
-					/*window.location.replace("???.html");*/
-				});
-	 
-				//Returns false for manual redirect to allow time for firebase query to run.
-				return false;
-			}
-		},
-		signInOptions: [
-			// Leave the lines as is for the providers you want to offer your users.
-			firebase.auth.GoogleAuthProvider.PROVIDER_ID
-		],
-		// Terms of service url.
-		tosUrl: '<your-tos-url>'
-	};
-	
-	// Initialize the FirebaseUI Widget using Firebase.
-	var ui = new firebaseui.auth.AuthUI(firebase.auth());
-	// The start method will wait until the DOM is loaded.
-	ui.start('#login-button', uiConfig);
+	var provider = new firebase.auth.GoogleAuthProvider();
 	
 	firebase.auth().onAuthStateChanged(function(user){
 		if(user){
@@ -45,6 +14,10 @@ if(!firebase){
 		}
 	});
 	
+	
+	$("#login-button").click(function(){
+		firebase.auth().signInWithRedirect(provider);
+	});
 	
 	$("#logout-button").click(function(){
 		firebase.auth().signOut().then(function() {
