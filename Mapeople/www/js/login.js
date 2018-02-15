ecbda1f3dd2a7215a9bd8ea2c9ae6f086da7b3ed
FireLemons <shenyang64@gmail.com>
2018-02-15 14:48:18 -0600

    login with google working

--- Mapeople/www/js/login.js
+++ Mapeople/www/js/login.js
@@ -32,7 +32,7 @@ if(!firebase){
 	// Initialize the FirebaseUI Widget using Firebase.
 	var ui = new firebaseui.auth.AuthUI(firebase.auth());
 	// The start method will wait until the DOM is loaded.
-	ui.start('#firebaseui-auth-container', uiConfig);
+	ui.start('#login-button', uiConfig);
 	
 	firebase.auth().onAuthStateChanged(function(user){
 		if(user){
